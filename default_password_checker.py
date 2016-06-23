1290223b65c3edafbfe74f35eabaa4dc2f69666f
bebeco77 <octo77@gmail.com>
2016-06-23 15:38:26 +0900

    multiple check test
    multiple check test

--- default_password_checker.py
+++ default_password_checker.py
@@ -5,10 +5,8 @@ import bander_checker
 import urllib
 import urllib2
 import random
-from multiprocessing import Process, Manager
+import os
 
-def rotate_ip():
-    return ip
 
 
 #file open and list create
@@ -30,16 +28,18 @@ def file_open():
     len_u = len(u)
     len_p = len(p)
 
-    global pre
+#    global pre
+    global pre,pre_1,pre_2,pre_11,pre_12,pre_21,pre_22
     pre=[]
 
     for i in ip:
         for j in u:
             for k in p:
                 pre.append(i +";"+ j+";" +k)
-#    print len(pre)
+
+
+
 # divide attack list
-#    pre_1 = Manager.list()
     pre_1 = pre[:len(pre)/2]
     pre_11 = pre_1[:len(pre_1)/2]
     pre_12 = pre_1[len(pre_1)/2:]
@@ -48,56 +48,60 @@ def file_open():
     pre_21 = pre_2[:len(pre_2)/2]
     pre_22 = pre_2[len(pre_2)/2:]
 
+    with open ("tmp\pre_11","w") as fp:
+        for line in pre_11:
+            fp.write(line+"\n")
+    with open ("tmp\pre_12","w") as fp:
+        for line in pre_12:
+            fp.write(line+"\n")
+    with open ("tmp\pre_21","w") as fp:
+        for line in pre_21:
+            fp.write(line+"\n")
+    with open ("tmp\pre_22","w") as fp:
+        for line in pre_22:
+            fp.write(line+"\n")
 
-#    print len(pre_1)
-#    print pre_1
-#
-#    print len(pre_11)
-#    print pre_11
-#    print len(pre_12)
-#    print pre_12
-#
-#    print '\n'
-#    print len(pre_2)
-#    print pre_2
-#    print len(pre_22)
-#    print pre_21
-#    print len(pre_22)
-#    print pre_22
-
-
-def brute(attack_list):
-    count=0
-    check_result=[]
-    for i in attack_list:
-        count +=1
-        bt_oj = i.split(';')
+#def brute(attack_list):
+#    print attack_list
+#    count=0
+#    check_result=[]
+#    for i in attack_list:
+#        count +=1
+#        bt_oj = i.split(';')
 #        print str(bt_oj)
 #        print i
-        check_result.append(bander_checker.bander_check(bt_oj[0],bt_oj[1],[bt_oj[2]]))
+#        check_result.append(bander_checker.bander_check(bt_oj[0],bt_oj[1],[bt_oj[2]]))
 #        if "Login OK" in check_result:
 #            print check_result
-    return check_result
+#    return check_result
 
 
 
 def main():
 
     file_open()
+#    print pre_1
     print "Scanning...\n"
-    list_brute = brute(pre)
-#    pre_1 = pre
-#    list_brute1= Process(target= brute, args = (pre_1))
+    os.system("python multiple_checker.py tmp/pre_11 >> tmp/pre&")
+    os.system("python multiple_checker.py tmp/pre_12 >> tmp/pre&")
+    os.system("python multiple_checker.py tmp/pre_21 >> tmp/pre&")
+    os.system("python multiple_checker.py tmp/pre_22 >> tmp/pre&")
 
-    print "\nLogin 'Fail' list \n ------------------------------------------"
-    for text in list_brute:
-        if "Fail" in text:
-            print text
 
-    print "\nLogin 'OK' list \n ------------------------------------------"
-    for text in list_brute:
-        if "OK" in text:
-            print text
+#    list_brute = r1
+
+
+################# print result
+
+#    print "\nLogin 'Fail' list \n ------------------------------------------"
+#    for text in list_brute:
+#        if "Fail" in text:
+#            print text
+
+#    print "\nLogin 'OK' list \n ------------------------------------------"
+#    for text in list_brute:
+#        if "OK" in text:
+#            print text
 
 if __name__ == '__main__':
     main()
---
+++ multiple_checker.py
@@ -0,0 +1,64 @@
+import module__ruby_l2
+import module__netgear_l2
+import module__belair_wifi
+import bander_checker
+import urllib
+import urllib2
+import random
+import os
+import sys
+
+
+dir = list(sys.argv)
+#print dir[1]
+
+
+#file open and list create
+def file_open():
+    global pre
+    pre=[]
+    f = open(dir[1], 'r')
+    pre = f.readlines()
+    f.close()
+
+    pre = [pres.replace("\n","") for pres in pre]
+
+#    print pre_11
+#    print pre_12
+#    print pre_21
+#    print pre_22
+#    p = [password.replace("\n","") for password in p]
+#    len_ip = len(ip)
+#    len_u = len(u)
+#    len_p = len(p)
+
+
+def brute(attack_list):
+#    print attack_list
+    count=0
+    check_result=[]
+    for i in attack_list:
+        count +=1
+        bt_oj = i.split(';')
+#        print str(bt_oj)
+#        print i
+        check_result.append(bander_checker.bander_check(bt_oj[0],bt_oj[1],[bt_oj[2]]))
+#        if "Login OK" in check_result:
+#            print check_result
+    return check_result
+
+
+
+def main():
+
+
+    file_open()
+#    print pre
+    brute(pre)
+#    print str(sys.argv)
+#    print "Scanning...\n"
+
+
+
+if __name__ == '__main__':
+    main()
